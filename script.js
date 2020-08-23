7cd54cdc956592109f6f0f705d3695c13c5ec048
cvlgeek <70110028+cvlgeek@users.noreply.github.com>
2020-08-23 16:57:30 -0600

    Update script.js

--- script.js
+++ script.js
@@ -6,15 +6,8 @@ function getTimestamp(){
 	var xhttp = new XMLHttpRequest();
 	xhttp.onreadystatechange = function(){
 		if (this.readyState == 4){
-			var s = this.status;
 			if (this.status == 200){
 				alert xhttp.responseText.substr(xhttp.responseText.indexOf('epoch":')+7,10);
-			}
-			else{
-				var e = new Error();
-				e.name = '';
-				e.description = '';
-				return e;
 			}
 		}
 	};
