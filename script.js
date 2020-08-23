750070120191fdd14aa7238b47cf9203838814da
cvlgeek <70110028+cvlgeek@users.noreply.github.com>
2020-08-23 16:44:04 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
 function run(){
-  alert("hello world");
+  getTimestamp();
+}
+
+function getTimestamp(){
+	var xhttp = new XMLHttpRequest();
+	xhttp.onreadystatechange = function(){
+		if (this.readyState == 4){
+			var s = this.status;
+			if (this.status == 200){
+				alert xhttp.responseText.substr(xhttp.responseText.indexOf('epoch":')+7,10);
+			}
+			else{
+				var e = new Error();
+				e.name = '';
+				e.description = '';
+				return e;
+			}
+		}
+	};
+	xhttp.open("GET", "https://api.pro.coinbase.com/time", false);
+	xhttp.send();
 }
